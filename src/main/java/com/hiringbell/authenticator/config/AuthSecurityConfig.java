fa9513c9f52df002fd9c83d9f6ed0e0cf6d6acfd
istiyakmi9 <istiyaq.mi9@gmail.com>
2024-02-12 14:58:59 +0530

    reactive removed

--- src/main/java/com/hiringbell/authenticator/config/AuthSecurityConfig.java
+++ src/main/java/com/hiringbell/authenticator/config/AuthSecurityConfig.java
@@ -2,13 +2,9 @@ package com.hiringbell.authenticator.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.jwt.JwtDecoders;
-import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 
 @Configuration
 @EnableWebFluxSecurity
